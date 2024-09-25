79cc7bebadde7f3f69dc8a01e93750ca19920077
AnyMilko <milko.ann@mail.ru>
2024-09-25 18:38:20 +0300

    Add files via upload

---
+++ lesson5.py
@@ -0,0 +1,12 @@
+immutable_var=(1,2.3, True, [3])
+print(immutable_var)
+
+#mutable_var[0] = "4"
+#print(mutable_var)# элементы кортежа не изменяемые
+
+
+mutable_list = ["pen", "pensil", "map"]
+print(mutable_list)
+print(mutable_list[0])
+mutable_list[0] = "list"
+print(mutable_list)
